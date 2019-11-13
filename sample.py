60227f0b4957baac1e1e02a8b3695d858131da4c
tristaaa <tristaaa56@126.com>
2019-11-12 17:03:38 -0800

    spark problem, output the nextPageUrl user visited per event during their ad jounery

---
+++ sample.py
@@ -0,0 +1,24 @@
+import sys
+import json
+from pyspark import SparkContext
+
+def task(argv):
+    infile, outfile = argv[1], argv[2]
+
+    sc = SparkContext('local[*]','task1')
+    adevRDD = sc.textFile(infile)
+
+    adevRDD2 = adevRDD.map(lambda x: json.loads(x))\
+        .map(lambda x: (x.get("visitorId"),x.get("id"),x.get("timestamp"),x.get("type"),x.get("pageUrl")))
+    validrecords = adevRDD2.groupBy(lambda x: x[0])\
+        .filter(lambda x: x[0] and len(x[1])>1).values()
+    ouputRDD = validrecords.map(list).map(lambda x: sorted(x,key=lambda i:i[2]))\
+        .map(lambda x: [e1+(e2[-1],) for e1,e2 in zip(x,x[1:])]).flatMap(lambda x: x)
+    ouputRDD = ouputRDD.map(lambda x:{"id":x[1],"timestamp":x[2],"type":x[3],"visitorId":x[0],"pageUrl":x[4],"nextPageUrl":x[5]})
+
+    with open(outfile,'w') as outf:
+        for output in ouputRDD.collect():
+            outf.write(json.dumps(output)+"'\n")
+
+if __name__ == '__main__':
+    task(sys.argv)
